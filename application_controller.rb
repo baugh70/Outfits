53fa01ea2d76fef4630405727090c49205d59082
IndyArkara <managarn17@gmail.com>
2015-07-16 17:28:28 +0000

    linked index and controller

--- application_controller.rb
+++ application_controller.rb
@@ -4,12 +4,15 @@ require_relative 'models/outfit.rb'
 
 class MyApp < Sinatra::Base
 
+ 
+  
   puts '/' do
     @outfit = Outfit.new(params["zip_code"])
     erb :index
   end
 
-  get '/outfit.erb' do
+  post '/outfit' do
+    @outfit = Outfit.new(params["zip_code"])
     erb :outfit
   end
 end
